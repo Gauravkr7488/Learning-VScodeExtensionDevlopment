33b42a0483fc8fe52df14fdade9a731dfe68274d
Gaurav Kumar <gauravkr748800@gmail.com>
2025-02-18 19:32:35 +0530

    added axios and sent a POST req

--- project-4-Webservice-Calls/F2VsCodeExtensionPoC/extension.js
+++ project-4-Webservice-Calls/F2VsCodeExtensionPoC/extension.js
@@ -1,10 +1,12 @@
 const vscode = require('vscode');
+const axios = require('axios');
+
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-	const disposable = vscode.commands.registerCommand('extension-poc.GetWord', function () {
+	const disposable = vscode.commands.registerCommand('extension-poc.GetWord', async function () {
 
 		const editor = vscode.window.activeTextEditor;
 		const selection = editor.selection;
@@ -13,6 +15,16 @@ function activate(context) {
 		if (wordRange) {
 			const word = editor.document.getText(wordRange);
 			vscode.window.showInformationMessage(`Focused Word: ${word}`);
+
+			let url = "http://localhost:5229/api/string"; // add the url
+
+			try {
+				const response = await axios.post(url, { word });
+				vscode.window.showInformationMessage(`Response: ${response.data}`);
+			} catch (error) {
+				vscode.window.showErrorMessage(`API Error: ${error.message}`);
+			}
+
 		} else {
 			// If no word is found under the cursor, show a different message.
 			vscode.window.showInformationMessage("No word found under cursor.");
