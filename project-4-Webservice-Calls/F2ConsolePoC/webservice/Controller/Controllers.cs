3419ca7b1846198ed417e9561efb1e815b1a8bf2
Gaurav Kumar <gauravkr748800@gmail.com>
2025-02-18 14:37:12 +0530

    modified controllers

--- project-4-Webservice-Calls/F2ConsolePoC/webservice/Controller/Controllers.cs
+++ project-4-Webservice-Calls/F2ConsolePoC/webservice/Controller/Controllers.cs
@@ -10,8 +10,9 @@ public class StringController : ControllerBase
     private static readonly List<string> strings = new();
 
     [HttpPost]
-    public IActionResult PostString([FromBody] string message)
+    public IActionResult PostString([FromBody] dynamic body)
     {
+        string message = body.ToString();
         strings.Add(message);
         return Ok($"Recived String: {message}");
     }
