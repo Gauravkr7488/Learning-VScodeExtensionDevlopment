ed566ed94aaf289d3ff2ba2ecac6a03b80c8078e
Gaurav Kumar <gauravkr748800@gmail.com>
2025-02-18 15:22:03 +0530

    added writing on console

--- project-4-Webservice-Calls/F2ConsolePoC/webservice/Controller/Controllers.cs
+++ project-4-Webservice-Calls/F2ConsolePoC/webservice/Controller/Controllers.cs
@@ -10,11 +10,14 @@ public class StringController : ControllerBase
     private static readonly List<string> strings = new();
 
     [HttpPost]
-    public IActionResult PostString([FromBody] dynamic body)
+    public IActionResult PostString([FromBody] StringMessage body)
     {
-        string message = body.ToString();
+        
+        // string message = body.ToString();
+        string message = body.String;
         strings.Clear();
         strings.Add(message);
+        Console.WriteLine($"Recived String: {message}");
         return Ok($"Recived String: {message}");
     }
 
@@ -23,4 +26,9 @@ public class StringController : ControllerBase
     {
         return Ok(strings);
     }
+}
+
+public class StringMessage
+{
+    public required string String { get; set; }
 }
