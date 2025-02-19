74bfc85586592fd207686cd7390ae113d68e8e4c
Gaurav Kumar <gauravkr748800@gmail.com>
2025-02-19 20:06:13 +0530

    added the function to run server in the extension.js

--- Webservice_Host/F2VsCodeExtensionPoC/f2vscodeextensionpoc/extension.js
+++ Webservice_Host/F2VsCodeExtensionPoC/f2vscodeextensionpoc/extension.js
@@ -1,34 +1,24 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-
+const { StartServer } = require('./server.js');
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "f2vscodeextensionpoc" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
+async function activate(context) {
+	try {
+		await StartServer(); // Ensure server starts before proceeding
+		console.log("Server started successfully");
+	} catch (error) {
+		console.error("Failed to start server:", error);
+	}
 	const disposable = vscode.commands.registerCommand('f2vscodeextensionpoc.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from F2VsCodeExtensionPoC!');
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
